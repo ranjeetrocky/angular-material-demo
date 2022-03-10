9253e45d4adc3b780d2779e683685c509f2604eb
ranjeetrocky <ranjeetrocky2000@gmail.com>
2022-03-10 14:32:39 +0530

    9 added material icons

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,8 +2,13 @@ import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatIconModule } from '@angular/material/icon';
 
-const MaterialComponents = [MatButtonModule, MatButtonToggleModule];
+const MaterialComponents = [
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatIconModule,
+];
 
 @NgModule({
   imports: [MaterialComponents],
