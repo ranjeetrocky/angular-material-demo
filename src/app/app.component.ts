59e2e2bccc236ad7a5be1cafc87c15efdb8611b5
ranjeetrocky <ranjeetrocky2000@gmail.com>
2022-03-11 14:33:03 +0530

    23.2 filtered option

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { map, Observable, startWith } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -19,11 +21,24 @@ export class AppComponent {
     { name: 'AngularMaterial' },
     { name: 'Vue' },
   ];
+  myControl = new FormControl();
+  filteredOptions: Observable<string[]>;
+
+  _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.options.filter((option) =>
+      option.toLowerCase().includes(filterValue)
+    );
+  }
 
   constructor() {
     setInterval(() => {
       this.progress <= 100 ? this.progress++ : (this.progress = 0);
     }, 16);
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map((value) => this._filter(value))
+    );
   }
   log(string: string) {
     console.log(string);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,6 +15,7 @@ import { FormsModule } from '@angular/forms';
     BrowserAnimationsModule,
     MaterialModule,
     FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
