218bf8201ee74b8ce75752be8a8e27b3283f07d3
ranjeetrocky <ranjeetrocky2000@gmail.com>
2022-03-11 14:38:22 +0530

    24 checkbox

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -19,6 +19,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 const MaterialComponents = [
   MatButtonModule,
@@ -40,6 +41,7 @@ const MaterialComponents = [
   MatFormFieldModule,
   MatSelectModule,
   MatAutocompleteModule,
+  MatCheckboxModule,
 ];
 
 @NgModule({
