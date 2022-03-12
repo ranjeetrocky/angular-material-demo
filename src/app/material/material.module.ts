bf04b9b834c2e238bfae8756b54930a983b35b57
ranjeetrocky <ranjeetrocky2000@gmail.com>
2022-03-12 12:24:08 +0530

    31 drag

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -27,6 +27,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatDialogModule } from '@angular/material/dialog';
 import { ScrollingModule } from '@angular/cdk/scrolling';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 const MaterialComponents = [
   MatButtonModule,
@@ -56,6 +57,7 @@ const MaterialComponents = [
   MatSnackBarModule,
   MatDialogModule,
   ScrollingModule,
+  DragDropModule,
 ];
 
 @NgModule({
